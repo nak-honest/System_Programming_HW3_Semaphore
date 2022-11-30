4d00809c717b9d9775abef55ccb6c45d411c782c
nhlee98 <e5slnh9876@gmail.com>
2022-11-30 20:47:04 +0900

    code update

--- scheduler.c
+++ scheduler.c
@@ -14,34 +14,59 @@ void __ContextSwitch(int curpid, int newpid) {
     kill(newpid, SIGCONT);
 }
 
-void remove_sem(int sig) {
+void remove_sem_exit(int sig) {
     sem_unlink("mysem");
     sem_close(SEM);
     exit(0);
 }
 
+void remove_sem_abort(int sig) {
+    sem_unlink("mysem");
+    sem_close(SEM);
+    signal(SIGABRT, SIG_DFL);
+    abort();
+}
+
 void scheduler(int signo, siginfo_t *info, void *context) {
+    /*
     if (ReadyQueue.queueCount != 0 && pCurrentThread != NULL) {
         kill(pCurrentThread->pid, SIGSTOP);
     }
+    */
     SEM = sem_open("mysem", O_CREAT, 0644, 1);
 
     sem_wait(SEM);
     /*
-        for (int i = 0; i < 1000000; i++) {
-            int j = i;
-        }
+    for (int i = 0; i < 1000000; i++) {
+        int j = i;
+    }
     */
+    char buf[512] = "Hello";
+    /*
+    if(pCurrentThread != NULL) {
+        char cur[] = "Curthread : ";
+        for (int i = 0; i++; i < sizeof(cur)) {
+            buf[i] =
+            }
+    }
+    */
+    int fd = open("test.txt", O_CREAT, 0644);
+    write(fd, buf, sizeof(buf));
+
     Thread *cur_thread, *new_thread;
     pid_t curpid, newpid;
 
     if (ReadyQueue.queueCount == 0) {
+        if (pCurrentThread != NULL) {
+            if (pCurrentThread->status == THREAD_STATUS_ZOMBIE) {
+                pCurrentThread = NULL;
+            } else {
+                pCurrentThread->cpu_time += 2;
+            }
+        }
         sem_post(SEM);
-        sem_close(SEM);
-        sem_unlink("mysem");
         return;
     } else if (pCurrentThread != NULL) {
-        // __ContextSwitch(pCurrentThread->pid, ReadyQueue.pHead->pid);
         new_thread = ReadyQueue.pHead;
         cur_thread = pCurrentThread;
         pCurrentThread = new_thread;
@@ -50,8 +75,12 @@ void scheduler(int signo, siginfo_t *info, void *context) {
         curpid = cur_thread->pid;
         newpid = new_thread->pid;
 
+        __ContextSwitch(curpid, newpid);
+
         queue_pop(&ReadyQueue);
-        new_thread->status = THREAD_STATUS_RUN;
+        if (new_thread->status == THREAD_STATUS_READY) {
+            new_thread->status = THREAD_STATUS_RUN;
+        }
 
         if (cur_thread->status != THREAD_STATUS_ZOMBIE) {
             queue_push(&ReadyQueue, cur_thread);
@@ -63,11 +92,11 @@ void scheduler(int signo, siginfo_t *info, void *context) {
         pCurrentThread->cpu_time += TIMESLICE;
         queue_pop(&ReadyQueue);
         pCurrentThread->status = THREAD_STATUS_RUN;
-        // kill(pCurrentThread->pid, SIGCONT);
+        kill(pCurrentThread->pid, SIGCONT);
     }
 
     sem_post(SEM);
-    kill(pCurrentThread->pid, SIGCONT);
+    // kill(pCurrentThread->pid, SIGCONT);
 }
 
 void RunScheduler(void) {
@@ -82,12 +111,13 @@ void RunScheduler(void) {
     sigemptyset(&act.sa_mask);
     sigaction(SIGALRM, &act, NULL);
     timer_create(CLOCK_REALTIME, NULL, &timer_id);
-    signal(SIGINT, remove_sem);
+    signal(SIGINT, remove_sem_exit);
+    signal(SIGABRT, remove_sem_abort);
 
-    value.it_interval.tv_sec = 1;
-    value.it_interval.tv_nsec = 999 * 1000 * 1000;
-    value.it_value.tv_sec = 1;
-    value.it_value.tv_nsec = 999 * 1000 * 1000;
+    value.it_interval.tv_sec = TIMESLICE;
+    value.it_interval.tv_nsec = 0;
+    value.it_value.tv_sec = TIMESLICE;
+    value.it_value.tv_nsec = 0;
 
     if (pCurrentThread == NULL && ReadyQueue.queueCount != 0) {
         sem_wait(SEM);
--- thread.c
+++ thread.c
@@ -14,22 +14,22 @@
 ThreadQueue ReadyQueue;
 ThreadQueue WaitingQueue;
 ThreadTblEnt pThreadTbEnt[MAX_THREAD_NUM];
-Thread *pCurrentThread; // Running 상태의 Thread를 가리키는 변수
+Thread *pCurrentThread;
 
 void Init(void) {
-    /* ReadyQueue 초기화 */
+    sem_close(SEM);
+    sem_unlink("mysem");
+
     ReadyQueue.queueCount = 0;
     ReadyQueue.pHead = NULL;
     ReadyQueue.pTail = NULL;
 
-    /* WaitingQueue 초기화 */
     WaitingQueue.queueCount = 0;
     WaitingQueue.pHead = NULL;
     WaitingQueue.pTail = NULL;
 
-    pCurrentThread = NULL; // pCurrentThread 초기화
+    pCurrentThread = NULL;
 
-    /* pThreadTbEnt 멤버 초기화 */
     for (int i = 0; i < MAX_THREAD_NUM; i++) {
         pThreadTbEnt[i].bUsed = 0;
         pThreadTbEnt[i].pThread = NULL;
@@ -38,7 +38,7 @@ void Init(void) {
 int thread_create(thread_t *thread, thread_attr_t *attr,
                   void *(*start_routine)(void *), void *arg) {
     SEM = sem_open("mysem", O_CREAT, 0644, 1);
-    sem_wait(SEM); // 전역 변수에 대한 동기화
+    sem_wait(SEM);
 
     Thread *new_thread;
     void *stack;
@@ -46,17 +46,16 @@ int thread_create(thread_t *thread, thread_attr_t *attr,
     pid_t new_pid;
     BOOL is_full = 1;
 
-    /* Table의 entry 0부터 빈 entry를 찾는다 */
     for (int i = 0; i < MAX_THREAD_NUM; i++) {
         if (pThreadTbEnt[i].bUsed == 0) {
             is_full = 0;
-            tid = i;       // 해당 엔트리의 번호가 thread의 id임.
-            *thread = tid; // 생성될 thread id 반환
+            tid = i;
+            *thread = tid;
             break;
         }
     }
 
-    if (is_full == 1) { // Table의 빈 엔트리가 없는 경우
+    if (is_full == 1) {
         perror("The number of creatable threads are full!");
         sem_post(SEM);
         return -1;
@@ -65,22 +64,20 @@ int thread_create(thread_t *thread, thread_attr_t *attr,
     stack = malloc(STACK_SIZE);
     new_thread = (Thread *)malloc(sizeof(Thread));
 
-    // 스레드 생성 후 pid 반환
-    new_pid = clone(start_routine, (char *)stack + STACK_SIZE,
-                    CLONE_VM | CLONE_SIGHAND | CLONE_FS | CLONE_FILES, arg);
+    new_pid =
+        clone(start_routine, (char *)stack + STACK_SIZE,
+              SIGCHLD | CLONE_VM | CLONE_SIGHAND | CLONE_FS | CLONE_FILES, arg);
 
-    kill(new_pid, SIGSTOP); // 스레드를 생성 후 바로 정지시킨다
+    kill(new_pid, SIGSTOP);
 
-    /* TCB 초기화 */
     new_thread->stackSize = STACK_SIZE;
     new_thread->stackAddr = stack;
     new_thread->status = THREAD_STATUS_READY;
     new_thread->pid = new_pid;
     new_thread->cpu_time = 0;
 
-    queue_push(&ReadyQueue, new_thread); // ReadyQueue에 push
+    queue_push(&ReadyQueue, new_thread);
 
-    /* Table에 update */
     pThreadTbEnt[tid].pThread = new_thread;
     pThreadTbEnt[tid].bUsed = 1;
 
@@ -91,30 +88,26 @@ int thread_create(thread_t *thread, thread_attr_t *attr,
 
 int thread_suspend(thread_t tid) {
     SEM = sem_open("mysem", O_CREAT, 0644, 1);
-    sem_wait(SEM); // 전역 변수에 대한 동기화
+    sem_wait(SEM);
 
-    // 해당되는 tid의 스레드가 존재하지 않음
     if (pThreadTbEnt[tid].bUsed == 0) {
         perror("thread_suspend() : That tid is not exist!");
         sem_post(SEM);
         return -1;
     }
 
-    // Table의 tid 엔트리에서 스레드의 TCB 포인터를 얻어온다.
     Thread *thread = pThreadTbEnt[tid].pThread;
 
-    // 자기 자신은 suspend 하지 않도록 구현
     if (pCurrentThread == thread) {
         perror("thread_suspend() : self-suspend!");
         sem_post(SEM);
         return -1;
     }
 
-    // 해당 스레드가 Ready 상태인 경우 ReadyQueue에서 WaitingQueue로 이동
     if (thread->status == THREAD_STATUS_READY) {
         queue_remove(&ReadyQueue, thread);
         thread->status = THREAD_STATUS_WAIT;
-        queue_push(&WaitingQueue, thread); // Waiting Queue의 tail로 이동
+        queue_push(&WaitingQueue, thread);
     }
 
     sem_post(SEM);
@@ -123,20 +116,17 @@ int thread_suspend(thread_t tid) {
 
 int thread_cancel(thread_t tid) {
     SEM = sem_open("mysem", O_CREAT, 0644, 1);
-    sem_wait(SEM); // 전역 변수에 대한 동기화
+    sem_wait(SEM);
 
-    // 해당되는 tid의 스레드가 존재하지 않음
     if (pThreadTbEnt[tid].bUsed == 0) {
         perror("thread_resume() : That tid is not exist!");
         sem_post(SEM);
         return -1;
     }
 
-    // Table의 tid 엔트리에서 스레드의 TCB 포인터를 얻어온다.
     Thread *thread = pThreadTbEnt[tid].pThread;
     pid_t tpid = thread->pid;
 
-    // 자기 자신은 cancle 하지 않도록 구현
     if (pCurrentThread == thread) {
         perror("thread_cancel() : self-cancel!");
         sem_post(SEM);
@@ -146,18 +136,15 @@ int thread_cancel(thread_t tid) {
 
     kill(tpid, SIGKILL);
 
-    /* Queue에서 제거 */
     if (thread->status == THREAD_STATUS_READY) {
         queue_remove(&ReadyQueue, thread);
     } else if (thread->status == THREAD_STATUS_WAIT) {
         queue_remove(&WaitingQueue, thread);
     }
 
-    /* Table에서 해당 스레드 제거 */
     pThreadTbEnt[tid].bUsed = 0;
     pThreadTbEnt[tid].pThread = NULL;
 
-    /* Stack 및 TCB를 deallocate */
     free(thread->stackAddr);
     thread->stackAddr = NULL;
     free(thread);
@@ -168,24 +155,21 @@ int thread_cancel(thread_t tid) {
 }
 
 int thread_resume(thread_t tid) {
-    SEM = sem_open("mysem", O_CREAT, 0644, 1); // 전역 변수에 대한 동기화
+    SEM = sem_open("mysem", O_CREAT, 0644, 1);
     sem_wait(SEM);
 
-    // 해당되는 tid의 스레드가 존재하지 않음
     if (pThreadTbEnt[tid].bUsed == 0) {
         perror("thread_resume() : That tid is not exist!");
         sem_post(SEM);
         return -1;
     }
 
-    // Table의 tid 엔트리에서 스레드의 TCB 포인터를 얻어온다.
     Thread *thread = pThreadTbEnt[tid].pThread;
 
-    /* WaitingQueue에서 ReadyQueue로 이동 */
     if (thread->status == THREAD_STATUS_WAIT) {
         queue_remove(&WaitingQueue, thread);
         thread->status = THREAD_STATUS_READY;
-        queue_push(&ReadyQueue, thread); // ReadyQueue의 tail로 이동
+        queue_push(&ReadyQueue, thread);
     }
 
     sem_post(SEM);
@@ -193,15 +177,14 @@ int thread_resume(thread_t tid) {
 }
 
 thread_t thread_self(void) {
-    SEM = sem_open("mysem", O_CREAT, 0644, 1); // 전역 변수에 대한 동기화
+    SEM = sem_open("mysem", O_CREAT, 0644, 1);
     sem_wait(SEM);
 
     thread_t tid;
 
-    /* Table의 모든 엔트리에 대해 TCB 주소 비교 */
     for (int i = 0; i < MAX_THREAD_NUM; i++) {
         if (pThreadTbEnt[i].pThread == pCurrentThread) {
-            tid = i; // 찾으면 tid 할당
+            tid = i;
         }
     }
 
@@ -212,70 +195,62 @@ thread_t thread_self(void) {
 void disjoin(int signo) {}
 
 int thread_join(thread_t tid) {
-    SEM = sem_open("mysem", O_CREAT, 0644, 1); // 전역 변수에 대한 동기화
+    SEM = sem_open("mysem", O_CREAT, 0644, 1);
     sem_wait(SEM);
 
     Thread *new_thread, *parent_thread, *child_thread;
     pid_t curpid, newpid;
 
-    parent_thread = pCurrentThread; // 현재 이 함수를 호출한 스레드가 부모임
-    child_thread = pThreadTbEnt[tid].pThread; // tid에 해당하는 스레드가 자식임
+    parent_thread = pCurrentThread;
+    child_thread = pThreadTbEnt[tid].pThread;
 
-    signal(SIGCHLD, disjoin); // SIGCHLD에 대한 시그널 등록
+    signal(SIGCHLD, disjoin);
 
-    /* parent를 WaitingQueue로 이동시킨다. */
     pCurrentThread->status = THREAD_STATUS_WAIT;
     queue_push(&WaitingQueue, parent_thread);
 
-    // ReadyQueue가 비어있지 않은 경우에만 새로운 스레드를 실행시킬수 있다
     if (ReadyQueue.queueCount != 0) {
         new_thread = ReadyQueue.pHead;
-        queue_pop(&ReadyQueue); // ReadyQueue의 head에서 스레드를 가져온다
+        queue_pop(&ReadyQueue);
 
-        /* 새로운 스레드 TCB 업데이트 */
         new_thread->status = THREAD_STATUS_RUN;
         new_thread->cpu_time += 2;
 
         newpid = new_thread->pid;
-        kill(newpid, SIGCONT);       // 새로운 스레드 실행
-        pCurrentThread = new_thread; // pCurrentThread 업데이트
+        kill(newpid, SIGCONT);
+        pCurrentThread = new_thread;
     } else {
-        pCurrentThread = NULL; // ReadyQueue가 비어있는 경우 아무것도 실행안함
+        pCurrentThread = NULL;
     }
 
-    sem_post(SEM); // parent가 pause 상태에 들어가기 때문에 동기화 해제
+    sem_post(SEM);
 
     while (child_thread->status != THREAD_STATUS_ZOMBIE) {
-        pause(); // SIGCHLD를 받으면 깨어나서 자식이 종료된건지 확인
+        pause();
     }
 
-    sem_wait(SEM); // parent가 깨어나면 다시 동기화를 건다
+    sem_wait(SEM);
 
-    /* parent를 WaitingQueue에서 ReadyQueue로 보낸다 */
     queue_remove(&WaitingQueue, parent_thread);
     queue_push(&ReadyQueue, parent_thread);
     parent_thread->status = THREAD_STATUS_READY;
 
-    // pCurrentThread = NULL; // 자식이 죽었으므로 현재 실행중인 스레드가 없도록
-
-    /* 자식 스레드의 Stack과 TCB를 deallocate */
     free(child_thread->stackAddr);
     child_thread->stackAddr = NULL;
     free(child_thread);
     child_thread = NULL;
 
-    // Table에서 child thread 삭제
     pThreadTbEnt[tid].bUsed = 0;
     pThreadTbEnt[tid].pThread = NULL;
 
     sem_post(SEM);
-    kill(parent_thread->pid, SIGSTOP); // 위의 작업을 모두 수행 후 자신을 정지
+    kill(parent_thread->pid, SIGSTOP);
 
     return 0;
 }
 
 int thread_cputime(void) {
-    SEM = sem_open("mysem", O_CREAT, 0644, 1); // 전역 변수에 대한 동기화
+    SEM = sem_open("mysem", O_CREAT, 0644, 1);
     sem_wait(SEM);
 
     int time = (int)pCurrentThread->cpu_time;
@@ -284,4 +259,11 @@ int thread_cputime(void) {
     return time;
 }
 
-void thread_exit(void) { pCurrentThread->status = THREAD_STATUS_ZOMBIE; }
+void thread_exit(void) {
+    SEM = sem_open("mysem", O_CREAT, 0644, 1);
+    sem_wait(SEM);
+
+    pCurrentThread->status = THREAD_STATUS_ZOMBIE;
+
+    sem_post(SEM);
+}
