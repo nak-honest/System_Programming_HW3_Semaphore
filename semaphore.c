4d9ff78470c386c5cd5112b5dec56bddb5c3f210
nhlee98 <e5slnh9876@gmail.com>
2022-12-18 03:02:24 +0900

    Add semaphore.h semaphore.c

---
+++ semaphore.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "thread.h"
+#include "semaphore.h"
+
+SemaphoreTblEnt pSemaphoreTblEnt[MAX_SEMAPHORE_NUM];
+
+int thread_sem_open(char* name, int count)
+{
+
+}
+
+
+int thread_sem_wait(int semid)
+{
+
+}
+
+int thread_sem_post(int semid)
+{
+
+}
+
+int thread_sem_close(int semid)
+{
+
+}
+
